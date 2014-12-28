9b6e635220baa23589b9feea03e5d7a1991f4358
Shannon Tsaltas <shannontsaltas@gmail.com>
2014-12-28 16:47:31 -0600

    returns error message if user creates post with wrong data type

--- posts/api.py
+++ posts/api.py
@@ -8,6 +8,15 @@ import decorators
 from posts import app
 from database import session
 
+# JSON Schema describing the structure of a post
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
@@ -34,6 +43,14 @@ def posts_get():
 def posts_post():
     """ Add a new post """
     data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not we return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
 
     # Add the post to the database
     post = models.Post(title=data["title"], body=data["body"])
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -224,5 +224,40 @@ class TestAPI(unittest.TestCase):
                          "Request must contain application/json data"
         )
 
+    def testInvalidData(self):
+        """ Posting a post with an invalid body """
+        data = {
+            "title": "Example Post",
+            "body": 32
+        }
+
+        response = self.client.post("/api/posts",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 422)
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "32 is not of type 'string'")
+
+    def testMissingData(self):
+        """ Posting a post with a missing body """
+        data = {
+            "title": "Example Post",
+        }
+
+        response = self.client.post("/api/posts",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 422)
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "'body' is a required property")
+
 if __name__ == "__main__":
     unittest.main()
